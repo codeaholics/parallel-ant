9d4e565abc5039a41bd2b403eeb833018a0e984a
Danny Yates <danny@codeaholics.org>
2010-05-06 12:49:30 +0000

    More tests

---
+++ src/test/org/codeaholics/tools/build/pant/DependencyGraphTest.java
@@ -0,0 +1,114 @@
+package org.codeaholics.tools.build.pant;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.tools.ant.Target;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.*;
+
+import static org.junit.Assert.*;
+
+@RunWith(JMock.class)
+public class DependencyGraphTest {
+    private static final String TARGET_NAME1 = "targetName1";
+    private static final String TARGET_NAME2 = "targetName2";
+    private static final String TARGET_NAME3 = "targetName3";
+    private static final String TARGET_NAME4 = "targetName4";
+
+    private Mockery mockery;
+    private DependencyGraphEntryFactory dependencyGraphEntryFactory;
+    private Map<String, Target> targetMap;
+
+    @Before
+    public void setUp() {
+        mockery = new Mockery();
+        mockery.setImposteriser(ClassImposteriser.INSTANCE);
+
+        dependencyGraphEntryFactory = mockery.mock(DependencyGraphEntryFactory.class);
+        targetMap = new HashMap<String, Target>();
+    }
+
+    @Test
+    public void testReturnsSameDependencyGraphEntryWhenFindingDependenciesForSameTarget() {
+        final Target target = createTargetAndPutInMap(TARGET_NAME1);
+        final DependencyGraphEntry dependencyGraphEntry = new DependencyGraphEntry(target, null, null);
+
+        mockery.checking(new Expectations() {{
+            one(dependencyGraphEntryFactory).create(target);  // only calls the factory once
+            will(returnValue(dependencyGraphEntry));
+        }});
+
+        final DependencyGraph dependencyGraph = new DependencyGraph(targetMap, dependencyGraphEntryFactory);
+
+        // both the first and second call for the same target return the same graph entry
+        assertThat(dependencyGraph.buildDependencies(target), sameInstance(dependencyGraphEntry));
+        assertThat(dependencyGraph.buildDependencies(target), sameInstance(dependencyGraphEntry));
+    }
+
+    @Test
+    public void testCorrectlyDeterminesSuccessors() {
+        final Target target1 = createTargetAndPutInMap(TARGET_NAME1);
+        final Target target2 = createTargetAndPutInMap(TARGET_NAME2, TARGET_NAME1);
+        final Target target3 = createTargetAndPutInMap(TARGET_NAME3, TARGET_NAME1);
+        final Target target4 = createTargetAndPutInMap(TARGET_NAME4, TARGET_NAME2, TARGET_NAME3);
+
+        final Set<String> expectedSuccessorsOfTarget1 = new HashSet<String>(Arrays.asList(TARGET_NAME3, TARGET_NAME2));
+
+        final CapturingDependencyGraphEntryFactory capturingDependencyGraphEntryFactory =
+            new CapturingDependencyGraphEntryFactory(null, null);
+
+        final DependencyGraph dependencyGraph = new DependencyGraph(targetMap, capturingDependencyGraphEntryFactory);
+
+        dependencyGraph.buildDependencies(target4);
+
+        assertThat(capturingDependencyGraphEntryFactory.get(TARGET_NAME1).getSuccessors(),
+                   equalTo(expectedSuccessorsOfTarget1));
+    }
+
+    private Target createTargetAndPutInMap(final String targetName, final String... dependencies) {
+        final Target target = mockery.mock(Target.class, "target:" + targetName);
+        mockery.checking(new Expectations() {{
+            allowing(target).getName();
+            will(returnValue(targetName));
+
+            allowing(target).getDependencies();
+            will(returnEnumeration(dependencies));
+        }});
+
+        targetMap.put(targetName, target);
+
+        return target;
+    }
+
+    private static final class CapturingDependencyGraphEntryFactory extends DependencyGraphEntryFactory {
+        private final Map<String, DependencyGraphEntry> capturedEntries;
+
+        public CapturingDependencyGraphEntryFactory(final TargetExecutionNotifier targetExecutionNotifier,
+                                                    final TargetExecutor targetExecutor) {
+            super(targetExecutionNotifier, targetExecutor);
+            capturedEntries = new HashMap<String, DependencyGraphEntry>();
+        }
+
+        @Override
+        public DependencyGraphEntry create(final Target target) {
+            final DependencyGraphEntry graphEntry = super.create(target);
+            capturedEntries.put(target.getName(), graphEntry);
+            return graphEntry;
+        }
+
+        public DependencyGraphEntry get(final String targetName) {
+            return capturedEntries.get(targetName);
+        }
+    }
+}
